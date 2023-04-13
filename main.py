7a2b6bb27f228ac1aed0fba0cebe7e1ef21553da
PRACHICUTE <130650659+PRACHICUTE@users.noreply.github.com>
2023-04-13 09:38:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import*
+
+window = Tk()
+window.geometry("400x400")
+window.title("simple calculator")
+
+lbl1 =Label(window,text="enter 1st Number:")
+lbl1.place(x=50,y=50)
+          
+lbl2 =Label(window,text="enter 2st Number:")
+lbl2.place(x=50,y=100)
+                   
