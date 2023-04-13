45c0ff4e2e8caff54145a080bc0f5e980f37028d
1HK20IS074__Prachi Nagar <130650659+PRACHICUTE@users.noreply.github.com>
2023-04-13 10:18:10 +0000

    completed the code

--- main.py
+++ main.py
@@ -31,3 +31,38 @@ t3.insert(END,str(sum))
 b1 =Button(window, text="Add",command=add)
 b1.place(x=50,y=200)
 
+
+def sub():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sub = num1+num2
+t3.insert(END,str(sub))
+
+b2 =Button(window, text="Subtract",command=sub)
+b2.place(x=120,y=200)
+
+def mul():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  mul = num1+num2
+t3.insert(END,str(mul))
+
+b3 =Button(window, text="Multiply",command=mul)
+b3.place(x=220,y=200)
+
+def div():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  div = num1+num2
+t3.insert(END,str(div))
+
+b4 =Button(window, text="Divide",command=div)
+b4.place(x=320,y=200)
+
+def clear():
+  t1.delete(0,END)
+  t2.delete(0,END)
+  t3.delete(0,END)
+
+b5 =Button(window, text="clear",command=clear)
+b5.place(x=180,y=250)
